95566f31cb205e6522a48d0b5b74d0143583448c
Brandon Benson <brandonbenson09@gmail.com>
2022-08-14 19:43:35 -0400

    asd

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/pwa-budgetting-flamingos";
 
 const app = express();
 
@@ -16,15 +17,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/pwa-budgetting-flamingos",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // routes
 app.use(require("./routes/api.js"));
